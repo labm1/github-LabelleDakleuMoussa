1d6ed38f2f29885c1b811fb25a27c0dd0781af3a
bilanmoussa <123095291+bilanmoussa@users.noreply.github.com>
2023-07-29 14:33:18 -0400

    constructeur

--- TimeLog/src/timelog/Employe.java
+++ TimeLog/src/timelog/Employe.java
@@ -1,5 +1,13 @@
 package timelog;
 
-public class Employe {
+import java.sql.Date;
+
+public class Employe extends Personne {
+
+	public Employe(String nom, int id_personne, int taux_horaire_base, int taux_horaire_supp, Date date_embauche,
+			Date date_depart, int numero_nas) {
+		super(nom, id_personne, taux_horaire_base, taux_horaire_supp, date_embauche, date_depart, numero_nas);
+		// TODO Auto-generated constructor stub
+	}
 
 }
